4f9087ee8fbd566a063e1a46c3a0122317f98da4
Chris Paul <cpaul319@users.noreply.github.com>
2019-04-14 13:43:39 -0400

    update js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,12 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+
+
+
+
 const db = mysql.createConnection({
-
-     
-
     host: "localhost",
     port: 3306,
     user: "root",
